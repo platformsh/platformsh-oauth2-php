399068e875567e8073dca7ea1669bbdb8a37a5f1
Patrick Dawkins <pjcdawkins@gmail.com>
2020-11-09 13:00:28 +0000

    Refactor config and remove use of password grant type

--- tests/Provider/PlatformshTest.php
+++ tests/Provider/PlatformshTest.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Psr7\Response;
 use function GuzzleHttp\Psr7\stream_for;
 use League\OAuth2\Client\Grant\Password;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use Platformsh\OAuth2\Client\Exception\TfaRequiredException;
 use Platformsh\OAuth2\Client\Grant\ApiToken;
 use Platformsh\OAuth2\Client\Provider\Platformsh;
 use Platformsh\OAuth2\Client\Tests\MockClient;
@@ -42,40 +41,6 @@ class PlatformshTest extends \PHPUnit_Framework_TestCase
         $provider->getAccessToken($grant, [
             'username' => 'foo',
             'password' => 'bar2',
-        ]);
-    }
-
-    public function testGetAccessTokenWithTfa()
-    {
-        $totpValue = 123456;
-        $mockResponse = function (RequestInterface $request) use ($totpValue) {
-            $totp = $request->getHeader(Platformsh::TFA_HEADER);
-            if (empty($totp)) {
-                return (new Response(401))
-                    ->withHeader(Platformsh::TFA_HEADER, 'required, schemes=totp')
-                    ->withBody(stream_for('{
-                    "error": "invalid_grant",
-                    "error_description": "Two-factor authentication is required."
-                    }'));
-            } elseif ($totp !== $totpValue) {
-                return (new Response(401))
-                    ->withBody(stream_for('{
-                    "error": "invalid_grant",
-                    "error_description": "Incorrect token value."
-                    }'));
-            }
-
-            return (new Response(200))
-                ->withHeader('Content-Type', 'application/json')
-                ->withBody(stream_for(json_encode(['access_token' => 123])));
-        };
-        $client = MockClient::withResponses([$mockResponse, $mockResponse]);
-        $provider = new Platformsh([], ['httpClient' => $client]);
-        $grant = new Password();
-        $this->expectException(TfaRequiredException::class);
-        $provider->getAccessToken($grant, [
-            'username' => 'foo',
-            'password' => 'bar',
         ]);
     }
 
